aa115b41bb3d751af5300248b358f2a603a0b45f
fuadsuleyman <fuadsuleymanov@outlook.com>
2022-06-10 05:21:32 +0400

    add button whitch in empty inputs show error alert

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
     <h2>Manage Goals</h2>
-    <input type="text" />
+    <input type="text"  ref="goal"/>
+    <button @click="setGoals">Set Goal</button>
   </div>
 </template>
+
+
+<script>
+export default {
+    methods: {
+        setGoals(){
+            if (this.$refs.goal.value === '')
+            alert("Input is Empty")
+        }
+    },
+}
+</script>

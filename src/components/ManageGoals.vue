0f80d8158e1082067a507ceba81264550724c590
fuadsuleyman <fuadsuleymanov@outlook.com>
2022-06-10 05:53:38 +0400

    add own error dialog and with vue build in teleport tag change his location in dom

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,19 +1,37 @@
 <template>
   <div>
     <h2>Manage Goals</h2>
-    <input type="text"  ref="goal"/>
+    <input type="text" ref="goal" />
     <button @click="setGoals">Set Goal</button>
+    <teleport to="body">
+      <alert-error v-if="inputisInvalid">
+        <h2>Input is Invalid</h2>
+        <p>Please add at least few characters...</p>
+        <button @click="confirmError">Okay</button>
+      </alert-error>
+    </teleport>
   </div>
 </template>
 
 
 <script>
+import AlertError from "./ErrorAlert.vue";
 export default {
-    methods: {
-        setGoals(){
-            if (this.$refs.goal.value === '')
-            alert("Input is Empty")
-        }
+  components: { AlertError },
+  data() {
+    return {
+      inputisInvalid: false,
+    };
+  },
+  methods: {
+    setGoals() {
+      if (this.$refs.goal.value === "") this.inputisInvalid = true;
+      console.log(`this.inputisInvalid: ${this.inputisInvalid}`);
     },
-}
+    confirmError() {
+      this.inputisInvalid = false;
+      console.log(`this.inputisInvalid: ${this.inputisInvalid}`);
+    },
+  },
+};
 </script>

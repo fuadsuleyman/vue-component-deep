fed498ced9b8c156bee6c5ba256ad68a6a1a98f5
fuadsuleyman <fuadsuleymanov@outlook.com>
2022-06-10 05:13:40 +0400

    keep-alive tag not remove component from dom, that is way we keep input values when switch beetween components

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,10 @@
     <button @click="swirchComponents('manage-goals')">Manage Goals</button>
     <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
     <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
-    <component :is="selectedComponent"></component>
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
+    
   </div>
 </template>
 

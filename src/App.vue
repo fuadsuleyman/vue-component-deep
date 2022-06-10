3c58329f697eca0ca7bfe146c44c5b4bbd3f6f45
fuadsuleyman <fuadsuleymanov@outlook.com>
2022-06-10 05:03:15 +0400

    we delete all components and two new com and show then by pressing button

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,37 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-    <course-goals>
-      <template #default="slotProps">
-        <h2>{{ slotProps.item }}</h2>
-        <h1>{{ slotProps['another-prop'] }}</h1>
-      </template>
-    </course-goals>
+    <button @click="swirchComponents('active-goals')">Active Goals</button>
+    <button @click="swirchComponents('manage-goals')">Manage Goals</button>
+    <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals>
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
-import BadgeList from "./components/BadgeList.vue";
-import UserInfo from "./components/UserInfo.vue";
-import CourseGoals from "./components/CourseGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import CourseGoals from "./components/CourseGoals.vue";
 export default {
-  components: { TheHeader, BadgeList, UserInfo, CourseGoals },
+  components: { TheHeader, ActiveGoals, ManageGoals },
   data() {
     return {
+      selectedComponent: "active-goals",
       activeUser: {
         name: "Maximilian Schwarzmüller",
         description: "Site owner and admin",
         role: "admin",
       },
     };
+  },
+  methods: {
+    swirchComponents(cmp) {
+      this.selectedComponent = cmp;
+      console.log(`this.selectedComponent: ${this.selectedComponent}`);
+    },
   },
 };
 </script>
